cf49dfdd3355bfbaa0c4b7aadce8455efe091343
Ian Strascina <istrasci@gmail.com>
2011-02-25 22:08:12 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mafia::Application.config.secret_token = 'a67a74523b7a1a176ab5d9c70c5c21022964746bb9f81285170865fa097d1b04273fdfd67617c10dcbdabeea4ee3189684845cdabef2d64d3041ab357c804671'
