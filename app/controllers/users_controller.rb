25fd64d59b1c69dd2ff8419489a9dacb7911f37e
Ian Strascina <istrasci@gmail.com>
2011-04-12 22:47:53 -0600

    Basic user registration setup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        #format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        format.html { redirect_to(:users, :notice => 'Registration Successful!') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
